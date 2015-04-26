21a359367eacd91916710f37690c2b41ed773fe7
jonathan.cairns <jonathan.cairns@intergen.co.nz>
2015-04-26 14:36:13 +1200

    Added test runner for all major browsers. Added test to check if session storage perferred over local storage

--- karma.conf.js
+++ karma.conf.js
@@ -59,8 +59,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
+    // browsers: ['Firefox','Chrome','IE','PhantomJS'], // for realeases
+    browsers: ['PhantomJS'], // for dev
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
--- test/service.spec.js
+++ test/service.spec.js
@@ -14,35 +14,63 @@ describe('TODO:LOL', function() {
   	localStorage.clear();
   });
 
-  it('should be able to set in local and session storage', function() {
-    var valueToBeSet = 'hello'
-    storage.set('testKey', valueToBeSet).then(function() {
-    	var browserSessionStorage = sessionStorage.getItem('intergen.testKey')
-    	expect(browserSessionStorage).toBe(valueToBeSet);
-    });
-    $timeout.flush();
-  });
+	it('should be able to set in local and session storage', function() {
+		var valueToBeSet = 'hello'
+		storage.set('testKey', valueToBeSet).then(function() {
+			var browserSessionStorage = sessionStorage.getItem('intergen.testKey')
+			expect(browserSessionStorage).toBe(valueToBeSet);
+		});
+		$timeout.flush();
+	});
 
-  it('should compress the target value and set in local and session storage', function() {
-  	storage.configure({
-  		useCompression: true
-  	});
+	it('should compress the target value and set in local and session storage', function() {
+		storage.configure({
+			useCompression: true
+		});
 
-  	var valueToBeSet = {
-  		a: 'adsfklsdfkmlsklfsdlkdsfklfsdlkjfsdklsfdkljsfdkjlsfdjklsfdjkfsdj dsfjfdsjlkfdsjlkfsdjlkjksldf j sfjkfds jlk sfdjkl sfd',
-  		b: 'adskldsl kfdjkslfjdslk jkdfsjksfd jfksdjk dfsjk fdskjfsdkjl fdskfkjlfdkjlfdkfskjljk sldfjk l sdfk js kjsfdkjlfsdjksfdjksfdkj l kjfs dkjlsf djkljkl fsdkjls'
-  	}
-  	var stringLength = JSON.stringify(valueToBeSet).length;
+		var valueToBeSet = {
+			a: 'adsfklsdfkmlsklfsdlkdsfklfsdlkjfsdklsfdkljsfdkjlsfdjklsfdjkfsdj dsfjfdsjlkfdsjlkfsdjlkjksldf j sfjkfds jlk sfdjkl sfd',
+			b: 'adskldsl kfdjkslfjdslk jkdfsjksfd jfksdjk dfsjk fdskjfsdkjl fdskfkjlfdkjlfdkfskjljk sldfjk l sdfk js kjsfdkjlfsdjksfdjksfdkj l kjfs dkjlsf djkljkl fsdkjls'
+		}
+		var stringLength = JSON.stringify(valueToBeSet).length;
 
-  	storage.set('compressionTest', valueToBeSet).then(function() {
-    	var browserSessionStorage = sessionStorage.getItem('intergen.compressionTest').length;
-    	expect(browserSessionStorage).toBeLessThan(stringLength)
-    	console.log('size before: ' + stringLength + ' size after:' +  browserSessionStorage);
-  	});
-  	$timeout.flush();
-  	storage.configure({
-  		useCompression: false
-  	});
-  })
+		storage.set('compressionTest', valueToBeSet).then(function() {
+			var browserSessionStorage = sessionStorage.getItem('intergen.compressionTest').length;
+			expect(browserSessionStorage).toBeLessThan(stringLength)
+			console.log('size before: ' + stringLength + ' size after:' +  browserSessionStorage);
+		});
+		$timeout.flush();
+		storage.configure({
+			useCompression: false
+		});
+	});
 
+	it('should get value from session storage', function() {
+		var value = {
+			a: 'blah'
+		};
+
+		sessionStorage.setItem('intergen.ses', value);
+
+		var fromSessionStorage = storage.get('ses');
+
+		expect(fromSessionStorage).not.toBe(null);
+		expect(fromSessionStorage).not.toBe(undefined);
+	});
+
+	it('should get value from storage - with session storage preference', function() {
+	  	var valueSession = {
+	  		a: 'blah'
+	  	};
+	  	var valueLocal = {
+	  		a: 'notblah'
+	  	};
+
+		sessionStorage.setItem('intergen.ses', JSON.stringify(valueSession));
+	  	localStorage.setItem('intergen.ses', JSON.stringify(valueLocal));
+
+	  	var fromSessionStorage = storage.get('ses');
+
+	  	expect(fromSessionStorage.a).toBe(valueSession.a);
+	});
 });
